816d43cc44294f8dcfa925157da4948a1a13538f
Lapa <ksuhosha@mail.ru>
2022-09-11 00:24:10 +0300

    оптимизация ....

--- MEDIUM+/Program.cs
+++ MEDIUM+/Program.cs
@@ -4,67 +4,46 @@ int length = 0;
 int tmp = num;
 List<int> listSimplNum = new();
 List<int> listUnderNum = new();
-while(tmp != 0)
+while (tmp != 0)
 {
     tmp /= 10;
     length++;
 }
-
-for(int i = 0; i < length - 1; i++)
+bool CheckSimplNum(int num)
+{
+    if (num < 4) return true;
+    else if (num % 2 == 0) return false;
+    else
+    {
+        int length = (int)Math.Sqrt(num) + 1;
+        for (int i = 3; i <= length; i += 2)
+        {
+            if (num % i == 0) return false;
+        }
+    }
+    return true;
+}
+for (int i = 0; i < length - 1; i++)
 {
 
-    for(int j = 0; j < length - i; j++)
+    for (int j = 0; j < length - i; j++)
     {
         tmp = num;
-        int div1 = (int)Math.Pow(10,length - j);
-        int div2 = (int)Math.Pow(10,length - (j + 1) - i);
+        int div1 = (int)Math.Pow(10, length - j);
+        int div2 = (int)Math.Pow(10, length - (j + 1) - i);
         tmp = tmp % div1 / div2;
-        if(listUnderNum.Count == 0)
+        if (!listUnderNum.Contains(tmp))
         {
             listUnderNum.Add(tmp);
         }
-        for(int k = 0; k < listUnderNum.Count; k++)
-        {
-            if(listUnderNum[k] == tmp)
-            {
-                break;
-            }
-            else if(k == listUnderNum.Count - 1)
-            {
-                listUnderNum.Add(tmp);
-            }
-        }
+
     }
 }
-
-for(int i = 0; i < listUnderNum.Count; i++)
+for (int i = 0; i < listUnderNum.Count; i++)
 {
-    if(listUnderNum[i] is 2 or 1 or 3)
-    {
-        listSimplNum.Add(listUnderNum[i]);
-    }
-    else if(listUnderNum[i] % 2 != 0)
-    {
-        length = (int)Math.Round(Math.Sqrt(listUnderNum[i]) + 1,0);
-        int count = 0;
-        for(int j = 1; j <= length; j += 2)
-        {
-            if(listUnderNum[i] % j == 0)
-            {
-                count++;
-            }
-            if(count == 2)
-            {
-                break;
-            }
-            else if(j == length - 1 || j == length)
-            {
-                listSimplNum.Add(listUnderNum[i]);
-            }
-        }
-    }
+    if (CheckSimplNum(listUnderNum[i])) listSimplNum.Add(listUnderNum[i]);
 }
 Console.WriteLine("список подчисел");
-Console.WriteLine(string.Join("\n",listUnderNum));
-Console.WriteLine("список натуральных чисел");
-Console.WriteLine(string.Join(", ",listSimplNum));
+Console.WriteLine(string.Join("\n", listUnderNum));
+Console.WriteLine("список простых чисел");
+Console.WriteLine(string.Join(", ", listSimplNum));
